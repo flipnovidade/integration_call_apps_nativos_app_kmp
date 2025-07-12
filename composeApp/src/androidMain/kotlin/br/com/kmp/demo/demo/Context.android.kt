0e67d5a911efc75486ef3b5e162133f036859328
Felippe Ferreira <felippe.ferreira@bbnk.com.br>
2025-07-12 21:04:10 +0100

    create context

---
+++ composeApp/src/androidMain/kotlin/br/com/kmp/demo/demo/Context.android.kt
@@ -0,0 +1,7 @@
+@file:Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+
+package br.com.kmp.demo.demo
+
+import android.app.Application
+
+actual typealias KMPContext = Application
---
+++ composeApp/src/commonMain/kotlin/br/com/kmp/demo/demo/Context.kt
@@ -0,0 +1,4 @@
+package br.com.kmp.demo.demo
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+expect class KMPContext
